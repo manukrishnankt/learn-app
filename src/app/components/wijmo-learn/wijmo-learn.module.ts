be55553b524a99e1ffece6420b24e55705d9b99e
manukrishnankt <manukrishnankt@gmail.com>
2020-09-04 01:32:15 +0530

    flex layout added

--- src/app/components/wijmo-learn/wijmo-learn.module.ts
+++ src/app/components/wijmo-learn/wijmo-learn.module.ts
@@ -3,13 +3,14 @@ import { CommonModule } from '@angular/common';
 
 import { WijmoLearnRoutingModule } from './wijmo-learn-routing.module';
 import { WijmoLearnComponent } from './wijmo-learn.component';
-
+import { FlexLayoutModule } from "@angular/flex-layout";
 
 @NgModule({
   declarations: [WijmoLearnComponent],
   imports: [
     CommonModule,
-    WijmoLearnRoutingModule
+    WijmoLearnRoutingModule,
+    FlexLayoutModule
   ]
 })
 export class WijmoLearnModule { }
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,15 +3,19 @@ import { CommonModule } from '@angular/common';
 import { BaselayoutComponent } from './dashboard/layout/baselayout/baselayout.component';
 import { TopNavComponent } from './dashboard/layout/top-nav/top-nav.component';
 import { SideNavComponent } from './dashboard/layout/side-nav/side-nav.component';
+import { FlexLayoutModule } from "@angular/flex-layout";
 
 import { HomeRoutingModule } from './home-routing.module';
+import { DemoMaterialModule } from './util/shared/material.module';
 
 
 @NgModule({
   declarations: [ BaselayoutComponent, TopNavComponent, SideNavComponent],
   imports: [
     CommonModule,
-    HomeRoutingModule
+    HomeRoutingModule,
+    FlexLayoutModule,
+    DemoMaterialModule
   ]
 })
 export class HomeModule { }
