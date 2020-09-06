7e8cf2a37bb57fa712f0f9e3be98e571d8a54b25
manukrishnankt <manukrishnankt@gmail.com>
2020-09-06 22:23:24 +0530

    Wijmo Intefrated

--- src/app/components/wijmo-learn/wijmo-learn.component.ts
+++ src/app/components/wijmo-learn/wijmo-learn.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ViewChild } from '@angular/core';
+
+import { WjFlexGridFilter } from '@grapecity/wijmo.angular2.grid.filter';
+import { CollectionView, IEventHandler } from '@grapecity/wijmo';
 
 @Component({
   selector: 'app-wijmo-learn',
@@ -7,9 +10,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WijmoLearnComponent implements OnInit {
 
-  constructor() { }
+  collectionView: CollectionView;
+  filterDefinition: string;
 
-  ngOnInit(): void {
+  @ViewChild('flexGridFilter') flexGridFilter: WjFlexGridFilter;
+
+  ngOnInit() {
+    this.collectionView = new CollectionView(this.getData());
   }
+
+  ngAfterViewInit() {
+  }
+
+  private getData() {
+    const countries = "US,Japan,UK,China,India,Italy,Greece".split(",");
+
+    return countries.map((country: string, index: number) => ({
+      id: index + 1,
+      country: country,
+      sales: Math.random() * 1000,
+      downloads: Math.random() * 500
+    }));
+  }s
 
 }
--- src/app/components/wijmo-learn/wijmo-learn.module.ts
+++ src/app/components/wijmo-learn/wijmo-learn.module.ts
@@ -4,13 +4,17 @@ import { CommonModule } from '@angular/common';
 import { WijmoLearnRoutingModule } from './wijmo-learn-routing.module';
 import { WijmoLearnComponent } from './wijmo-learn.component';
 import { FlexLayoutModule } from "@angular/flex-layout";
+import { WjGridModule } from '@grapecity/wijmo.angular2.grid';
+import { WjGridFilterModule } from '@grapecity/wijmo.angular2.grid.filter';
 
 @NgModule({
   declarations: [WijmoLearnComponent],
   imports: [
     CommonModule,
     WijmoLearnRoutingModule,
-    FlexLayoutModule
+    FlexLayoutModule,
+    WjGridModule, 
+    WjGridFilterModule
   ]
 })
 export class WijmoLearnModule { }
